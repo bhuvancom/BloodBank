d970528edfd1bc1a879c83740e542963cd2ce57b
Bhuvaneshvar nath Srivastava <bhuvaneshvar8@gmail.com>
2018-10-28 01:52:26 +0530

    added font for registration added add person button in main activity, clicking will take to registration page added regisration text in middle

--- app/src/main/java/com/newware/bloodbank/DonorRegistration.java
+++ app/src/main/java/com/newware/bloodbank/DonorRegistration.java
@@ -1,16 +1,53 @@
 package com.newware.bloodbank;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
 
 public class DonorRegistration extends AppCompatActivity
 {
+
+    RelativeLayout relativeLayout;
+    EditText etName, etAadhaar, etDOB, etPhone, etEmail;
+    RadioGroup gender;
+    RadioButton genderSex;
+    Spinner bloodGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donor_registration);
+        relativeLayout = findViewById(R.id.relativeRegister);
 //        getSupportActionBar().setElevation(0);
+        getUi();
+        final String[] bloodGroup = {"A+", "A-", "AB+", "AB-", "B+", "B-", "O-", "O+"};
+
+    }
+
+    private void getUi()
+    {
+        etName = findViewById(R.id.etName);
+        etAadhaar = findViewById(R.id.et_aadhaar);
+        etEmail = findViewById(R.id.et_email);
+        etPhone = findViewById(R.id.et_mobile);
+
+        bloodGroup = findViewById(R.id.spinnerBloodGroup);
+
+        gender = findViewById(R.id.gender); // radio group
+
+        int gen = gender.getCheckedRadioButtonId(); // get selected sex id
+        genderSex = findViewById(gen);// set identification to checked id
+
+    }
+
+    void showSnackBar(String str)
+    {
+        Snackbar.make(relativeLayout, str, Snackbar.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/newware/bloodbank/DonorRegistrationBean.java
@@ -0,0 +1,67 @@
+package com.newware.bloodbank;
+
+/**
+ * Created by Bhuvaneshvar Nath Srivastava on 28-10-2018 at 12:38 AM.
+ * Copyright (c) 2018
+ **/
+public class DonorRegistrationBean
+{
+    private String name;
+    private String gender;
+    private String bloodGroup;
+    private String contactNo;
+    private String dob;
+    private String aadhaar;
+    private String emailAddress;
+
+    public DonorRegistrationBean()
+    {
+    }
+
+    public DonorRegistrationBean(String name, String gender, String bloodGroup, String contactNo, String dob, String aadhaar, String emailAddress)
+    {
+        this.name = name;
+        this.gender = gender;
+        this.bloodGroup = bloodGroup;
+        this.contactNo = contactNo;
+        this.dob = dob;
+        this.aadhaar = aadhaar;
+        this.emailAddress = emailAddress;
+    }
+
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getGender()
+    {
+        return gender;
+    }
+
+    public String getBloodGroup()
+    {
+        return bloodGroup;
+    }
+
+    public String getContactNo()
+    {
+        return contactNo;
+    }
+
+    public String getDob()
+    {
+        return dob;
+    }
+
+    public String getAadhaar()
+    {
+        return aadhaar;
+    }
+
+    public String getEmailAddress()
+    {
+        return emailAddress;
+    }
+}
