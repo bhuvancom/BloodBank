ef6e5f7b9ad1c3b938c9573b6d5ec4ef86e2352b
Bhuvaneshvar nath Srivastava <bhuvaneshvar8@gmail.com>
2018-10-27 23:10:36 +0530

    added font for registration added add person button in main activity, clicking will take to registration page added regisration text in middle

--- app/src/main/java/com/newware/bloodbank/MainActivity.java
+++ app/src/main/java/com/newware/bloodbank/MainActivity.java
@@ -5,16 +5,35 @@ package com.newware.bloodbank;
  * Bhuvaneshvar Nath Srivastava
  **/
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
-
+    Button btnRegistartion;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUi();
+
+
+        btnRegistartion.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(MainActivity.this, DonorRegistration.class));
+            }
+        });
+    }
+
+    private void setUi()
+    {
+        btnRegistartion = findViewById(R.id.btnRegistartion);
     }
 }
